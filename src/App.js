f589fc976011b2f66681325531d926bcd273a817
PrajwalCC <b220035@iiit-bh.ac.in>
2022-07-21 23:19:24 +0530

    3rd update

--- src/App.js
+++ src/App.js
@@ -1,46 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./Components/Header";
+import Todos from "./Components/Todos";
+import Footer from "./Components/Footer";
+import  React from 'react';
+import AddTodo from './Components/AddTodo';
 
 function App() {
+
+  const onDelete = (todo) => {
+    console.log("I am onDelete", todo)
+    
+  
+    setTodos(todos.filter((e) =>{
+      return e!==todo;
+    }));
+  }
+  
+  const addTodo = (title ,desc ) => {
+    console.log(title , desc);
+    let sno = todos[todos.length -1].sno + 1;
+    const myTodo ={
+      sno :sno,
+      title: title,
+      desc: desc
+    }
+    setTodos([...todos , myTodo]);
+    console.log(todos);
+  }
+
+
+  const[todos, setTodos] = React.useState([
+    {
+      sno: 1,
+      title: "Complete DSA",
+      desc: "Practise Questions with theory"
+    },
+    {
+      sno: 2,
+      title: "Complete React",
+      desc: "Build some awesome frontend Projects"
+    },
+    {
+      sno: 3,
+      title: "Get start with Node JS after completing React",
+      desc: "Build major project of full-stack"
+    }
+  ])
+    
+
   return (
-    <div className="App">
-      <nav className="navbar navbar-expand-lg bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled">Disabled</a>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
-    </div>
+    <>
+      <Header title="My Todos-List" />
+      <AddTodo addTodo={addTodo}/>
+      <Todos todos ={todos} onDelete={onDelete}/>
+      <Footer />
+    </>
   );
 }
 
